980fa8160056b189124fb3191995df5147e34cc4
jaimelias <escribeme@jaimelias.com>
2021-06-18 15:56:33 -0500

    added clear button

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import { SelectField, Option, Button } from '@contentful/forma-36-react-components';
+import { HotTable } from '@handsontable/react';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import 'handsontable/dist/handsontable.full.css';
-import { HotTable } from '@handsontable/react';
-import Handsontable from 'handsontable';
 
+const defaultState = {
+	maxRows: 20,
+	data: [...Array(20)].map(r => [null])
+}
 
 class Field extends React.Component {
 	constructor(props){
 		super(props);
-		this.state = {
-			maxRows: 10,
-			colHeaders: ['Prices'],
-			data: [...Array(10)].map(r => [null])
-		};
+		const {sdk} = props;
+		const value = sdk.field.getValue();
+		this.colHeaders = ['Prices Per Person'];
+		this.state = (value) ? value : defaultState;
 		this.handleCellChange = this.handleCellChange.bind(this);
+		this.handleRowChange = this.handleRowChange.bind(this);
 	};
-	handleCellChange({changes, sdk, value, hot}){
-				
+	handleCellChange({changes, sdk, hot}){
+		
+		const value = this.state
+		
 		if(changes)
 		{
 			if(Array.isArray(changes))
@@ -35,31 +41,75 @@ class Field extends React.Component {
 					}
 				});
 																
-				sdk.field.setValue(value).then(v => console.log(v));
+				sdk.field.setValue(value).then(v => {
+					this.setState({...v});
+					console.log(v);
+				});
 			}
 		}
 	};
+	handleRowChange({changes, sdk}){
+		
+		const value = this.state
+		const newMaxRows = parseInt(changes.target.value);
+		const oldmaxRows = parseInt(value.maxRows);
+		
+		if(oldmaxRows !== newMaxRows)
+		{
+			
+			let data = value.data.filter((r, i) => (i+1) <= newMaxRows);
+			
+			if(newMaxRows > oldmaxRows)
+			{
+				const dif = newMaxRows-oldmaxRows;				
+				[...Array(dif)].forEach(r => data.push([null]));
+			}
+			
+			sdk.field.setValue({data, maxRows: newMaxRows}).then(v => {
+				this.setState({...v});
+				console.log(v);
+			});
+		}
+	};
+	handleClearValue({sdk}){
+		sdk.field.setValue(defaultState).then(v => {
+			this.setState({...v});
+			console.log(v);
+		});
+	}
 	render(){
 		const {sdk} = this.props;
-		const value = sdk.field.getValue() || this.state
-		const {colHeaders, data} = value;
+		const value = this.state
+		const {data, maxRows} = value;
 		
 		return(
 		
-			<div id={'hot-app'}>
+			<div id={'hot-app'} style={{height: '500px'}}>
+				
+				<Button onClick={()=>{this.handleClearValue({sdk})}} buttonType={'muted'}>Clear Field</Button>				
+				
+				<SelectField
+					value={maxRows}
+					labelText="Maximum Number of Prices Per Person" 
+					onChange={(changes)=>{this.handleRowChange({changes, sdk})}}>
+					{[...Array(20)].map((r, i) => <Option key={i} value={i+1}>{i+1}</Option>)}
+				</SelectField>
+				
 				<HotTable
 					licenseKey={'non-commercial-and-evaluation'}
 					data={data} 
-					colHeaders={colHeaders}
+					maxRows={maxRows}
+					colHeaders={this.colHeaders}
 					rowHeaders={true}
 					columns={[{type: 'numeric'}]}
 					width={'600'}
 					height={'300'}
-					afterChange={(changes)=> {this.handleCellChange({changes, sdk, value})}}
+					afterChange={(changes)=> {this.handleCellChange({changes, sdk})}}
 				/>
+				
 			</div>	
 		);
-	};
+	}; 
 };
 
 export default Field;
