02313d9ff25ee78dfc0645dff6490b695ea8dda0
jaimelias <escribeme@jaimelias.com>
2021-06-20 00:21:56 -0500

    fixed multi seasons

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SelectField, Option, Button } from '@contentful/forma-36-react-components';
+import { SelectField, Option, Button, Heading, SectionHeading, Form, FieldGroup} from '@contentful/forma-36-react-components';
 import { HotTable } from '@handsontable/react';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import 'handsontable/dist/handsontable.full.css';
@@ -8,13 +8,18 @@ const isNumber = val => !isNaN(Number(val));
 const column = {type: 'numeric'};
 const defaultState = {
 	maxRows: 2,
-	seasons: {season_1: [...Array(20)].map(r => [''])},
+	seasons: {season_1: {
+		fixed: [...Array(20)].map(r => ['']),
+		dynamic: [...Array(20)].map(r => [''])
+	}},
 	childrenFreeUpTo: 0,
 	childrenDiscount: 0,
 	womenPricing: 0,
 	colHeaders: ['Per Person'],
-	columns: [column]
+	columns: [column],
+	updateHeight: false
 }
+
 
 class Field extends React.Component {
 	constructor(props){
@@ -25,34 +30,31 @@ class Field extends React.Component {
 		this.handleCellChange = this.handleCellChange.bind(this);
 		this.handleRowChange = this.handleRowChange.bind(this);
 		this.handleVariablePricing = this.handleVariablePricing.bind(this);
+		this.forceUpdateHeight = this.forceUpdateHeight.bind(this);
+	};
+	forceUpdateHeight({sdk, thisWindow, updateHeight}){
+		if(updateHeight)
+		{
+			sdk.window.updateHeight(this.divRef.clientHeight);
+			
+			sdk.field.setValue({...this.state, updateHeight: false}).then(v => {
+				this.setState({...v});
+				console.log('height update to fit component');
+			});
+		}		
 	};
 	componentDidMount()
 	{
 		const {sdk} = this.props;
-				
-		if(window.innerHeight < this.divRef.clientHeight)
-		{
-			sdk.window.updateHeight(this.divRef.clientHeight);
-		}
-		else
-		{
-			sdk.window.updateHeight();
-		}
+		this.forceUpdateHeight({sdk, updateHeight: true, thisWindow: window});
 	};
 	componentDidUpdate()
 	{
 		const {sdk} = this.props;
-		
-		if(window.innerHeight < this.divRef.clientHeight)
-		{
-			sdk.window.updateHeight(this.divRef.clientHeight);
-		}
-		else
-		{
-			sdk.window.updateHeight();
-		}
+		const {updateHeight} = this.state;
+		this.forceUpdateHeight({sdk, updateHeight, thisWindow: window});
 	};	
-	handleCellChange({changes, sdk, hot}){
+	handleCellChange({changes, sdk, seasonId, priceType}){
 		
 		let {seasons} = {...this.state};
 		
@@ -63,15 +65,14 @@ class Field extends React.Component {
 				changes.forEach(o => {
 					
 					const [row, col, oldValue, newValue] = o;
-					
-					
+
 					if(oldValue !== newValue && !isNaN(oldValue) && !isNaN(newValue))
 					{
-						seasons.season_1[row][col] = newValue;
+						seasons[seasonId][priceType][row][col] = newValue;
 					}
 					else
 					{
-						seasons.season_1[row][col] = '';
+						seasons[seasonId][priceType][row][col] = '';
 					}
 				});
 																
@@ -109,15 +110,21 @@ class Field extends React.Component {
 		
 		if(oldmaxRows !== newMaxRows)
 		{
-			seasons.season_1 = seasons.season_1.filter((r, i) => (i+1) <= newMaxRows);
-			
-			if(newMaxRows > oldmaxRows)
+			for(let s in seasons)
 			{
-				const dif = newMaxRows-oldmaxRows;				
-				[...Array(dif)].forEach(r => seasons.season_1.push(addNulls));
+				for(let t in seasons[s])
+				{					
+					seasons[s][t] = seasons[s][t].filter((r, i) => (i+1) <= newMaxRows);
+					
+					if(newMaxRows > oldmaxRows)
+					{
+						const dif = newMaxRows-oldmaxRows;				
+						[...Array(dif)].forEach(r => seasons[s][t].push(addNulls));
+					}
+				}
 			}
 			
-			sdk.field.setValue({...this.state, seasons, maxRows: newMaxRows}).then(v => {
+			sdk.field.setValue({...this.state, seasons, maxRows: newMaxRows, updateHeight: true}).then(v => {
 				this.setState({...v});
 				console.log(v);
 			});
@@ -136,7 +143,6 @@ class Field extends React.Component {
 		
 		sdk.field.setValue({...this.state, [type]: change}).then(v => {
 			const {childrenDiscount, womenPricing} = v;
-			const seasons = {};
 			let addNull = 1;
 
 			let cols = {
@@ -176,84 +182,123 @@ class Field extends React.Component {
 			const colHeaders = Object.values(cols.colHeaders);
 			const columns = Object.values(cols.columns);
 
-			v.seasons.season_1.forEach(r => {
-				let d = r.filter((r, i) => (i+1) <= addNull);
-								
-				if(addNull > d.length)
+			for(let s in v.seasons)
+			{
+				for(let t in v.seasons[s])
 				{
-					const dif = addNull-d.length;				
-					[...Array(dif)].forEach(r => d.push(''));					
+					v.seasons[s][t].forEach(r => {
+						let d = r.filter((r, i) => (i+1) <= addNull);
+										
+						if(addNull > d.length)
+						{
+							const dif = addNull-d.length;				
+							[...Array(dif)].forEach(r => d.push(''));					
+						}
+						
+						v.seasons[s][t].push(d);
+					});					
 				}
-				
-				if(typeof seasons.season_1 === 'undefined')
-				{
-					seasons.season_1 = [];
-				}
-				seasons.season_1.push(d);
-			});
-			
-			sdk.field.setValue({...v, seasons, colHeaders, columns}).then(v2 => {
+			}
+
+			sdk.field.setValue({...v, seasons: v.seasons, colHeaders, columns}).then(v2 => {
 				this.setState({...v2});
 				console.log(this.state);
 			});
 		});		
 	};
+	
+	
 	render(){
 		const {sdk} = this.props;
 		const {seasons, maxRows, childrenFreeUpTo, childrenDiscount, womenPricing, colHeaders, columns} = this.state;
+		
+		const RenderHotTable = ({sdk, seasons, maxRows, colHeaders, columns}) => {
+			let output = [];
+			
+			const GetTable = ({seasonId, priceType}) => {
+				
+				return (
+					<HotTable
+						licenseKey={'non-commercial-and-evaluation'}
+						data={seasons[seasonId][priceType]} 
+						maxRows={maxRows}
+						colHeaders={colHeaders}
+						rowHeaders={true}
+						columns={columns}
+						width={'100%'}
+						afterChange={(changes)=> {this.handleCellChange({changes, sdk, seasonId, priceType})}}
+					/>				
+				);
+			};
+			
+			for(let k in seasons)
+			{
+				output.push(
+					<div id={k} key={k} className={'season-container'}>
+						<Heading>{k}</Heading>
+						<br/>
+						<SectionHeading>{'Fixed Price'} - {k}</SectionHeading>
+						<br/>
+						<GetTable seasonId={k} priceType={'fixed'} />
+						<br/>
+						<SectionHeading>{'Variable Price'} - {k}</SectionHeading>
+						<br/>
+						<GetTable seasonId={k} priceType={'dynamic'} />
+					</div>
+				);
+			}
+			
+			return output;
+		};
 				
 		return(
-		
-			<div
-				ref={element => this.divRef = element}
-				id={'hot-app'} >
-				
-				<Button onClick={()=>{this.handleClearValue({sdk})}} buttonType={'muted'}>Clear Field</Button>
-				
-				<SelectField
-					value={childrenFreeUpTo}
-					labelText={'Children free up to # (years old)'} 
-					onChange={(change) => {this.handleVariablePricing({sdk, type: 'childrenFreeUpTo', change})}}>
-					{[...Array(18)].map((r, i) => <Option key={i} value={i}>{i}</Option>)}
-				</SelectField>
-				
-				<SelectField
-					value={childrenDiscount}
-					labelText={'Children discount up to # (years old)'} 
-					onChange={(change) => {this.handleVariablePricing({sdk, type: 'childrenDiscount', change})}}>
-					{[...Array(18)].map((r, i) => <Option key={i} value={i}>{i}</Option>)}
-				</SelectField>
-				
-				<SelectField
-					value={womenPricing}
-					labelText={'Women Pricing'} 
-					onChange={(change) => {this.handleVariablePricing({sdk, type: 'womenPricing', change})}}>
-					<Option value={0}>{'Regular Rate'}</Option>
-					<Option value={1}>{'Grant Discount'}</Option>
-					<Option value={2}>{'Free of Cost'}</Option>
-				</SelectField>				
-				
-				<SelectField
-					value={maxRows}
-					labelText="Maximum Number of Prices Per Person" 
-					onChange={(changes)=>{this.handleRowChange({changes, sdk})}}>
-					{[...Array(20)].map((r, i) => <Option key={i} value={i+1}>{i+1}</Option>)}
-				</SelectField>
-				
-				<div className={'hot-container'}>
-				<HotTable
-					licenseKey={'non-commercial-and-evaluation'}
-					data={seasons.season_1} 
-					maxRows={maxRows}
-					colHeaders={colHeaders}
-					rowHeaders={true}
-					columns={columns}
-					width={'100%'}
-					afterChange={(changes)=> {this.handleCellChange({changes, sdk})}}
-				/>
-				</div>
-				
-			</div>	
+			<div ref={element => this.divRef = element} id={'hot-app'}>
+				<Form>
+					
+					<FieldGroup>
+						<Button onClick={()=>{this.handleClearValue({sdk})}} buttonType={'muted'}>Clear Field</Button>
+						
+						<SelectField
+							value={childrenFreeUpTo}
+							labelText={'Children free up to # (years old)'} 
+							onChange={(change) => {this.handleVariablePricing({sdk, type: 'childrenFreeUpTo', change})}}>
+							{[...Array(18)].map((r, i) => <Option key={i} value={i}>{i}</Option>)}
+						</SelectField>
+						
+						<SelectField
+							value={childrenDiscount}
+							labelText={'Children discount up to # (years old)'} 
+							onChange={(change) => {this.handleVariablePricing({sdk, type: 'childrenDiscount', change})}}>
+							{[...Array(18)].map((r, i) => <Option key={i} value={i}>{i}</Option>)}
+						</SelectField>
+						
+						<SelectField
+							value={womenPricing}
+							labelText={'Women Pricing'} 
+							onChange={(change) => {this.handleVariablePricing({sdk, type: 'womenPricing', change})}}>
+							<Option value={0}>{'Regular Rate'}</Option>
+							<Option value={1}>{'Grant Discount'}</Option>
+							<Option value={2}>{'Free of Cost'}</Option>
+						</SelectField>				
+						
+						<SelectField
+							value={maxRows}
+							labelText="Maximum Number of Prices Per Person" 
+							onChange={(changes)=>{this.handleRowChange({changes, sdk})}}>
+							{[...Array(20)].map((r, i) => <Option key={i} value={i+1}>{i+1}</Option>)}
+						</SelectField>
+					</FieldGroup>
+					
+					<RenderHotTable
+						sdk={sdk}
+						seasons={seasons}
+						maxRows={maxRows} 
+						colHeaders={colHeaders}
+						columns={columns}
+					/>
+					
+				</Form>
+			</div>
 		);
 	}; 
 };
